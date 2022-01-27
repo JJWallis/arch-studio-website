b6d4f91cda93637caaf2b8676c2d19c46a68183c
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 15:56:56 +0000

    Data fetched + users filtered over

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,9 +1,12 @@
-// getUsers - returns only names + email of users whose name contain letter k (still as arr of objs)
+// getUsers - returns only names + email of users whose name contains letter k (still as arr of objs)
 
 async function getUsers(endpoint: string) {
    const response = await fetch(endpoint)
    if (!response.ok) throw new Error(`fetch failed: ${response.status}`)
    const data = await response.json()
+   const users = data.filter((user) => user.name.toLowerCase().includes('k'))
+
+   console.log(data)
 }
 
-console.log(getUsers('https://jsonplaceholder.typicode.com/users'))
+getUsers('https://jsonplaceholder.typicode.com/users')
