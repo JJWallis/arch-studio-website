e43f3780c8be165cc684670627b459c31f53719d
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 14:25:37 +0000

    Pangram toy JS problem comments done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,5 +1,6 @@
-// pangram check function - Pangram is a str or sentence which contains all letters of English alphabet.
-// isPangram(str) - returns either true if str passed is Pangram or false if not.
+// pangram check function - Pangram is a str or sentence which contains all letters of English alphabet
+// using every letter of given alphabet at least once
+// isPangram(str) - returns either true if str passed is Pangram or false if not
 // Gotchas - str which contains capitals works with str that doesn't | str which contains spaces - trim before checking
 // console.log(isPangram('The quick brown fox jumps over the lazy dog')); // true
 // console.log(isPangram('This is not a pangram')); // false
