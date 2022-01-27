72a7d843f26e550713cd5aa57f56a102eb6d9eb5
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 16:15:52 +0000

    rain language problem comments ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,20 +1,2 @@
-// getUsers - returns only names + emails of users whose name contains letter k (still as arr of objs)
-
-async function getUsers(endpoint: string) {
-   const response = await fetch(endpoint)
-   const data = await response.json()
-   const users = data
-      .filter((user) => user.name.toLowerCase().includes('k'))
-      .map(({ name, email }) => {
-         return { name, email }
-      })
-   return users
-}
-
-;(async function () {
-   try {
-      console.log(await getUsers('https://jsonplaceholder.typicode.com/users'))
-   } catch (error) {
-      console.log(error.message)
-   }
-})()
+// rain language translator
+// toRainLanguage(int) - returns either str or num
