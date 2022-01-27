ddb6fd07b55da5b2cfe5a01d5122ab0cc840ebc1
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 16:00:58 +0000

    Working solution

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,12 +1,15 @@
-// getUsers - returns only names + email of users whose name contains letter k (still as arr of objs)
+// getUsers - returns only names + emails of users whose name contains letter k (still as arr of objs)
 
 async function getUsers(endpoint: string) {
    const response = await fetch(endpoint)
    if (!response.ok) throw new Error(`fetch failed: ${response.status}`)
    const data = await response.json()
-   const users = data.filter((user) => user.name.toLowerCase().includes('k'))
+   const users = data
+      .filter((user) => user.name.toLowerCase().includes('k'))
+      .map((user) => `${user.name} | (${user.email})`)
 
    console.log(data)
+   console.log(users)
 }
 
 getUsers('https://jsonplaceholder.typicode.com/users')
