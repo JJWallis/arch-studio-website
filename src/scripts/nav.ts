50edc78503cf457b3a340c5b27ded3b74f5f8bd2
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 14:31:46 +0000

    Logic problems ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,6 +1,10 @@
-// pangram check function - Pangram is a str or sentence which contains all letters of English alphabet
-// using every letter of given alphabet at least once
+// pangram check function - Pangram is a str or sentence which contains all letters of English alphabet at least once
 // isPangram(str) - returns either true if str passed is Pangram or false if not
 // Gotchas - str which contains capitals works with str that doesn't | str which contains spaces - trim before checking
 // console.log(isPangram('The quick brown fox jumps over the lazy dog')); // true
 // console.log(isPangram('This is not a pangram')); // false
+
+function isPangram(str: string) {}
+
+// initialse empty array
+// split() input str + loop over to add each letter to an array if it isn't already there
