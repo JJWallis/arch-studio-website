39f325a6547292279aea1ab05a7db871b71d4469
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-27 14:18:38 +0000

    Pangram toy JS problem comments ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,2 +1,2 @@
 // pangram check function - Pangram is a str or sentence which contains all letters of English alphabet.
-//
+// isPangram(str) - returns either true if str passed is Pangram or false if not.
