deb5829e10119534b5d9e9b6ead93a3be801999b
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-28 13:21:35 +0000

    Rain language translator comments done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,2 +1,7 @@
 // rain language translator
+// 1st case - number is divisible by 3 (3 as a factor) without remainder = output "Pling"
+// 2nd case - number is divisible by 5 (5 as a factor) without remainder = output "Plang"
+// 3rd case - number is divisible by 7 (7 as a factor) without remainder = output "Plong"
+// 4th case - number is divisible by any combo of numbers without remainder = output "PlingPlong" (21 = ex)
+// 5th case - number is not divisible by any combo of numbers without remainder = output the number itself
 // toRainLanguage(int) - returns either str or num
