dab285fd33655a8277697205cb5c736148f51df6
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-28 14:07:28 +0000

    isogram solution

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,9 +1,13 @@
-function toRainLanguage(numInput: number) {
-   let output = ''
-   if (numInput % 3 === 0) output += 'Pling'
-   if (numInput % 5 === 0) output += 'Plang'
-   if (numInput % 7 === 0) output += 'Plong'
-   return output || numInput
+// Isogram check function
+// isIsogram(str) + returns true if the string is an isogram, false otherwise
+// isogram - str which has every letter appearing only once within the string (no repeated letters in str itself)
+// Case - capital letters
+
+function isIsogram(inputStr: string) {
+   const str = inputStr.toLowerCase().split('')
+   const duplicates = new Set(str).size
+   console.log(str)
+   return str.length === duplicates || false
 }
 
-console.log(toRainLanguage(21))
+console.log(isIsogram('Dermatoglyphicsd')) // true
