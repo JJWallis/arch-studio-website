2873f0b618c1fede0731f47d60150692cec0b69b
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-28 13:48:42 +0000

    rain language solution added

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,16 +1,9 @@
-// toRainLanguage(int) - returns either str or num
-// 1st case - number is divisible by 3 (3 as a factor) without remainder = output "Pling"
-// 2nd case - number is divisible by 5 (5 as a factor) without remainder = output "Plang"
-// 3rd case - number is divisible by 7 (7 as a factor) without remainder = output "Plong"
-// 4th case - number is divisible by any combo of numbers without remainder = output "PlingPlong" (21 = ex)
-// 5th case - number is not divisible by any combo of numbers without remainder = output the number itself
-
-const map = new Map().set(3, 'Pling').set(5, 'Plang').set(7, 'Plong')
-
 function toRainLanguage(numInput: number) {
-   const numbers = [...map.keys()]
-   const results = numbers.filter((num) => numInput % num === 0)
-   return results.map((num) => map.get(num)).join('') || numInput
+   let output = ''
+   if (numInput % 3 === 0) output += 'Pling'
+   if (numInput % 5 === 0) output += 'Plang'
+   if (numInput % 7 === 0) output += 'Plong'
+   return output || numInput
 }
 
-console.log(toRainLanguage(10))
+console.log(toRainLanguage(21))
