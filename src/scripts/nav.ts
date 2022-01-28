668434eb2da625f872efe9c7943e968ee7708cd0
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-28 14:26:36 +0000

    isogram solution added

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -3,11 +3,10 @@
 // isogram - str which has every letter appearing only once within the string (no repeated letters in str itself)
 // Case - capital letters
 
-function isIsogram(inputStr: string) {
-   const str = inputStr.toLowerCase().split('')
-   const duplicates = new Set(str).size
-   console.log(str)
-   return str.length === duplicates || false
+const isIsogram = (string) => {
+   const lowerCased = string.toLowerCase()
+   const result = lowerCased
+      .split('')
+      .every((v, i) => console.log(v, i) || lowerCased.indexOf(v) === i)
+   return result
 }
-
-console.log(isIsogram('Dermatoglyphicsd')) // true
