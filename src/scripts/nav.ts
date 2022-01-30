d7015fef486b6737ff9ed7438696203fc578486f
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-30 16:48:28 +0000

    Solution + final comments added

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,7 +1,8 @@
 // leap year = divisble by 4 without remainder (occurs every 4 years)
 // except for century years that are not divisible by 400 (1700 = false return)
 // century leap year is evenly divisible by 400 - (ex: 1600 = true return as still a leap year)
-// Edge cases: divisible by 4, 100, years which aren't divisible by 400
+// year that is evenly divisible by 100 - leap year only if it is also evenly divisible by 400.
+// This is because they are evenly divisible by 100 but not by 400
 
 function isLeapYear(year: string) {
    const yearNum = parseInt(year, 10)
