7458c0f40910bf801b32117733d38d14bb936550
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-30 16:18:46 +0000

    Leap year edge cases and function syntax done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,3 +2,10 @@
 // leap year = divisble by 4 without remainder (occurs every 4 years)
 // except for century years that are not divisible by 400 (ex: 1700 = false return)
 // but not when century years are divisible by 400 (ex: 1600 = true return as still a leap year)
+// Edge cases: divisible by 4, 100, years which aren't divisible by 400
+
+function isLeapYear(year: string) {
+   return true
+}
+
+console.log(isLeapYear('2000'))
