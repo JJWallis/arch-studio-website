f38be9d2354246c6937de883065d018608b20c0f
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-30 16:29:47 +0000

    Comments updated and logic ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,11 +1,15 @@
-// isLeapYear(strOfYear) + returns true or false
 // leap year = divisble by 4 without remainder (occurs every 4 years)
-// except for century years that are not divisible by 400 (ex: 1700 = false return)
-// but not when century years are divisible by 400 (ex: 1600 = true return as still a leap year)
+// except for century years that are divisible by 400 (1700 = false return)
+// (ex: 1600 = true return as still a leap year)
+// evenly divisible by 400
 // Edge cases: divisible by 4, 100, years which aren't divisible by 400
 
 function isLeapYear(year: string) {
-   return true
+   const yearNum = parseInt(year, 10)
+   if (yearNum % 4 === 0) {
+      return true
+   }
+   //    return true
 }
 
-console.log(isLeapYear('2000'))
+console.log(isLeapYear('1700'))
