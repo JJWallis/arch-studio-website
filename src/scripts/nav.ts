030fd63e18589957a39719a88e67464d53cae222
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-30 16:16:12 +0000

    Leap year function comments ip

---
+++ src/scripts/nav.ts
@@ -0,0 +1,4 @@
+// isLeapYear(strOfYear) + returns true or false
+// leap year = divisble by 4 without remainder (occurs every 4 years)
+// except for century years that are not divisible by 400 (ex: 1700 = false return)
+// but not when century years are divisible by 400 (ex: 1600 = true return as still a leap year)
