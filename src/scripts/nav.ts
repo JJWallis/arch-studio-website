152f6f0123fd10fd170f344d1e7f3771afa7255e
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-31 14:17:00 +0000

    SOlution ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,6 +2,12 @@
 // ex: console.log(encode('wwwiiuuuu')); // 3w2i4u
 // ex: console.log(decode('3w2i4u')); // wwwiiuuuu
 
-function encode(input: string) {}
+function encode(input: string) {
+   const inputArr = input.split('').sort()
+   console.log(inputArr)
+   console.log(inputArr.filter((item, idx, arr) => item === arr[idx + 1]))
+}
+
+console.log(encode('wwwiiuuuu')) // 3w2i4u
 
 function decode(input: string) {}
