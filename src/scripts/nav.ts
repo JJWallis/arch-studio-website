e8efe9cf57023b574928c532f2389a38b7136e0f
JJWallis <josh.jamesonwallis@gmail.com>
2022-01-31 14:48:37 +0000

    decode solution ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,7 +1,3 @@
-// consecutive data els are replaced by a single data val + count of that val
-// ex: console.log(encode('wwwiiuuuu')); // 3w2i4u
-// ex: console.log(decode('3w2i4u')); // wwwiiuuuu
-
 function encode(input: string) {
    const inputArr = input
       .replace(/(\w)\1+/g, (match) => `${match.length} ${match[0]}`)
@@ -13,3 +9,5 @@ function encode(input: string) {
 console.log(encode('wwwiiuuuu')) // 3w2i4u
 
 function decode(input: string) {}
+
+console.log(decode('3w2i4u')) // wwwiiuuuu
