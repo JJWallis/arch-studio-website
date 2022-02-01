7967a9e48151a9522c7829f954ced74151aef300
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-01 14:30:48 +0000

    Flatten arr with reduce solution ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,7 +2,9 @@
 // console.log(makeFlat(["one", ["two", "three"]], ["four", ["five"]]))
 
 function makeFlat(arrOne: [string, string[]], arrTwo: [string, string[]]) {
-   return arrOne.flat()
+   return arrOne.reduce((acc, curr) => {
+      return null
+   }, [])
 }
 
 console.log(makeFlat(['one', ['two', 'three']], ['four', ['five']]))
