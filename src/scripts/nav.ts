f05e6651dcd57fef1e6a4a13df67b0e7ade5ef31
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-01 15:26:16 +0000

    Transpose matrix solution ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,7 +2,13 @@
 // transposeMatrix([[1,2,3], [1,2,3], [1,2,3]]) returns [[1,1,1], [2,2,2], [3,3,3]]
 
 function transposeMatrix(arr: any) {
-   return arr
+   const results = []
+   arr.map(([val]) => val)
+   return results
+   // map() over each arr + return a new array with el at desired index + 1 each time
+   // reduce + push() to accumilator until run out els (arrs) in original arr
+   // returns new arr with all els at corresponding index
+   // recursivley call function with index of curr val + 1 (grab from reduce)
 }
 
 console.log(
