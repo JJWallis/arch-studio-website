74d2f1b707394ba2bc2c46d6a7440bf9bb51a192
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-01 17:43:36 +0000

    Font sizes ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,14 +1,14 @@
-// make rows cols
-// transposeMatrix([[1, 2, 3], [1,2,3], [1,2,3]]) returns [[1,1,1], [2,2,2], [3,3,3]]
+// // make rows cols
+// // transposeMatrix([[1, 2, 3], [1,2,3], [1,2,3]]) returns [[1,1,1], [2,2,2], [3,3,3]]
 
-function transposeMatrix(arr: any) {
-   return arr.map((_, idx) => arr.map((v) => v[idx]))
-}
+// function transposeMatrix(arr: any) {
+//    return arr.map((_, idx) => arr.map((v) => v[idx]))
+// }
 
-console.log(
-   transposeMatrix([
-      [1, 2, 3],
-      [1, 2, 3],
-      [1, 2, 3],
-   ])
-)
+// console.log(
+//    transposeMatrix([
+//       [1, 2, 3],
+//       [1, 2, 3],
+//       [1, 2, 3],
+//    ])
+// )
