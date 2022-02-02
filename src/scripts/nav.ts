81559152f86f5cbc00f6b5388f3494c0058da119
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-02 20:38:20 +0000

    Fizzbuzz calculator challenge ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,4 +1,7 @@
-function encode(input: string) {}
+// Given an integer n, return a string array answer (1-indexed) where:
+//     answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//     answer[i] == "Fizz" if i is divisible by 3.
+//     answer[i] == "Buzz" if i is divisible by 5.
+//     answer[i] == i (as a string) if none of the above conditions are true.
 
-console.log(encode('wwwiiuuuu'))
-// console.log(decode('3w2i4u'))
+function fizzBuzzCalculator(int: number) {}
