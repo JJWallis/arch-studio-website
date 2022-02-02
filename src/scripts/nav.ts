9196a55b9b41a28699086af89b364d2fb4f0b373
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-02 14:20:54 +0000

    transpose matrix func ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,14 +1,9 @@
-// // make rows cols
-// // transposeMatrix([[1, 2, 3], [1,2,3], [1,2,3]]) returns [[1,1,1], [2,2,2], [3,3,3]]
+function transposeMatrix(arr: number[][]) {}
 
-// function transposeMatrix(arr: any) {
-//    return arr.map((_, idx) => arr.map((v) => v[idx]))
-// }
-
-// console.log(
-//    transposeMatrix([
-//       [1, 2, 3],
-//       [1, 2, 3],
-//       [1, 2, 3],
-//    ])
-// )
+console.log(
+   transposeMatrix([
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+   ])
+)
