2d5548015fe9cebe5bf031398f62f08d0fd8b334
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-02 20:53:23 +0000

    fizzbuzz comments done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -4,4 +4,17 @@
 //     answer[i] == "Buzz" if i is divisible by 5.
 //     answer[i] == i (as a string) if none of the above conditions are true.
 
-function fizzBuzzCalculator(int: number) {}
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+function fizzBuzzCalculator(n: number): string[] {
+   return []
+}
+
+console.log(fizzBuzzCalculator(15))
