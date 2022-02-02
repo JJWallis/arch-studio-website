72e3c4a4dbf0c83aed3122ea5406af644b4cf54e
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-02 15:27:43 +0000

    getUsers func ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,7 +2,11 @@ async function getUsers(url: string) {
    try {
       const data = await fetch(url)
       if (!data.ok) throw new Error('Failed to fetch users')
-      const results = data.json()
+      const results = await data.json()
+      const users = results.map(
+         ({ name, email }: any) => name.includes('k') && { name, email }
+      )
+      console.log(users)
    } catch (error) {
       console.error(error.message)
    }
