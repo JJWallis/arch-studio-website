f446c126a1d013ef88d246f55bf31a92d394768b
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-02 22:41:41 +0000

    fizzbuzz comments logic done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,20 +1,15 @@
-// Given an integer n, return a string array answer (1-indexed) where:
-//     answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-//     answer[i] == "Fizz" if i is divisible by 3.
-//     answer[i] == "Buzz" if i is divisible by 5.
-//     answer[i] == i (as a string) if none of the above conditions are true.
-
 // Input: n = 3
 // Output: ["1","2","Fizz"]
 
-// Input: n = 5
-// Output: ["1","2","Fizz","4","Buzz"]
-
-// Input: n = 15
-// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
-
-function fizzBuzzCalculator(n: number): string[] {
-   return []
+function fizzBuzzCalculator(n: number) {
+   const arr = []
+   for (let i = 1; i <= n; i++) {
+      let str = ''
+      if (i % 3 === 0) str += 'Fizz'
+      if (i % 5 === 0) str += 'Buzz'
+      arr.push(str ? str : i.toString())
+   }
+   return arr
 }
 
-console.log(fizzBuzzCalculator(15))
+console.log(fizzBuzzCalculator(5))
