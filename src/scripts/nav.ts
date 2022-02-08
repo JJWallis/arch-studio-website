dbcf295a2fbdadc3c278fbe6cad0584b4a45f747
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-08 15:12:01 +0000

    Leetcode twoSum problem done

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,12 +1,1 @@
-function twoSum(nums: number[], target: number) {
-   // can't use the same element twice
-   // only 1 solution
-   // return in any order
-   return nums
-      .map((numTop) =>
-         nums.findIndex((num) => num !== numTop && num + numTop === target)
-      )
-      .filter((num) => num !== -1)
-}
-
-console.log(twoSum([2, 7, 11, 15], 13))
+// OOP Style
