c133f4922250e979962199ce0e8dac70885f38d8
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-08 13:15:25 +0000

    Leetcode recursive number problem ip

---
+++ src/scripts/nav.ts
@@ -0,0 +1,11 @@
+// Given an integer num
+// repeatedly add all its digits until the result has only one digit + return it
+// edgcase --- 0 <= num <= 231 - 1
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2
+
+function addDigits(num: number) {}
