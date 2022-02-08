448ab798a57313633efbcdd404ff01c6b043b2f3
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-08 14:09:41 +0000

    Leetcode twoSum problem ip

---
+++ src/scripts/nav.ts
@@ -0,0 +1,11 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+function twoSum(nums: number[], target: number) {
+   // can't use the same element twice
+   // only 1 solution
+   // return in any order
+   return []
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
