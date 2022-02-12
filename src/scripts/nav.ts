d28ffb1caca6be654b9b65221d50ad257ad2b9f1
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-12 20:25:09 +0000

    nav ts logic added

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,2 +1,13 @@
 // OOP Style
 // mathematical operators -
+
+const hamburger = document.getElementById(
+   'hamburger-toggle'
+) as HTMLButtonElement
+const navMb = document.querySelector('.nav--pm') as HTMLElement
+
+hamburger.addEventListener('click', (e) => {
+   const status = navMb.getAttribute('data-visible')
+   navMb.setAttribute('data-visible', status === 'true' ? 'false' : 'true')
+   console.log(status)
+})
