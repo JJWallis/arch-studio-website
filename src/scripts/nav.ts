51d03eff78e163dc7902a6a8d56a01bdb17ffede
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-13 09:13:22 +0000

    nav script comments adde

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,6 +1,3 @@
-// OOP Style
-// mathematical operators -
-
 const hamburger = document.getElementById(
    'hamburger-toggle'
 ) as HTMLButtonElement
@@ -11,3 +8,10 @@ hamburger.addEventListener('click', (e) => {
    navMb.setAttribute('data-visible', status === 'true' ? 'false' : 'true')
    console.log(status)
 })
+
+// OOP Style
+// Understand problem (repeat it with diff words)
+// devise a plan - take lager issue + break down into smaller
+// execute plan - from outline of plan via comments
+// review solution - better approach?, can go back if run into issue
+// mathematical operators -
