4855a1a699981533a4df22974e3c53a6e543b1e1
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-16 16:40:24 +0000

    leetcode problem removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -14,25 +14,3 @@ hamburger.addEventListener('click', () => {
 // devise a plan - take lager issue + break down into smaller
 // execute plan - from outline of plan via comments
 // review solution - better approach?, can go back if run into issue
-
-// Input: nums = [0,0,1,1,1,2,2,3,3,4]
-// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-// Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively
-// It does not matter what you leave beyond the returned k (hence they are underscores)
-
-function removeDuplicates(nums: (number | string)[]) {
-   if (nums.length === 0) return 0
-   nums.sort()
-
-   let i = 0
-   for (let j = 1; j < nums.length; j++) {
-      if (nums[j] !== nums[i]) {
-         i++
-         nums[i] = nums[j]
-         console.log('running')
-      }
-   }
-   return i + 1
-}
-
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
