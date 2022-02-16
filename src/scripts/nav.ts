10c146222a03974142dea28cd213c6098aaa9f2f
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-16 16:49:35 +0000

    buildArr leetcode problem

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -12,5 +12,17 @@ hamburger.addEventListener('click', () => {
 // OOP Style
 // Understand problem (repeat it with diff words)
 // devise a plan - take lager issue + break down into smaller
+// what do I currently know
 // execute plan - from outline of plan via comments
 // review solution - better approach?, can go back if run into issue
+
+// Input: nums = [0,2,1,5,3,4]
+// Output: [0,1,2,4,5,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//  = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+//  = [0,1,2,4,5,3]
+
+function buildArray(nums: number[]): number[] {
+   return []
+}
