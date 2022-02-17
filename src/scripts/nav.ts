fc7cebb18fcb81a1c1d12348a3b81ae7b6ec2d10
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-17 22:22:40 +0000

    code exmaple removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,19 +15,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
-
-// There is a programming language with only four operations and one variable X:
-//  ++X and X++ increments the value of the variable X by 1.
-//  --X and X-- decrements the value of the variable X by 1.
-// Initially, the value of X is 0.
-// Given an array of strings operations containing a list of operations,
-// return the final value of X after performing all the operations.
-
-function finalValueAfterOperations(operations: string[]) {
-   return operations.reduce(
-      (acc, curr) => (curr.includes('--') ? acc - 1 : acc + 1),
-      0
-   )
-}
-
-console.log(finalValueAfterOperations(['--X', 'X++', 'X++']))
