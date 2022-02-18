0b3fe20a7d5ce72cef2ef25b811a7b56fd4cb819
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 14:10:03 +0000

    leetcode word duplication setup ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -19,7 +19,17 @@ hamburger.addEventListener('click', () => {
 // A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
 // You are given an array of strings sentences, where each sentences[i] represents a single sentence.
 // Return the maximum number of words that appear in a single sentence.
+// Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+// Output: 6
 
 function mostWordsFound(sentences: string[]): number {
    return 0
 }
+
+console.log(
+   mostWordsFound([
+      'alice and bob love leetcode',
+      'i think so too',
+      'this is great thanks very much',
+   ])
+) // 6
