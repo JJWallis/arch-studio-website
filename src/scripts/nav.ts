dc91d558dcafd863865f499a1cb04903036aa99f
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 14:18:41 +0000

    leetcode maxWord count removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,32 +15,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
-
-// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
-// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
-// Return the maximum number of words that appear in a single sentence.
-// Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
-// Output: 6
-
-function mostWordsFound(sentences: string[]) {
-   if (
-      sentences.length === 0 ||
-      !sentences.every(
-         (sentence) =>
-            sentence.includes(' ') &&
-            sentence[0] !== ' ' &&
-            sentence[sentence.length - 1] !== ' '
-      )
-   )
-      return 0
-   sentences.forEach((sentence) => sentence.toLowerCase())
-   return Math.max(...sentences.map((sentence) => sentence.split(' ').length))
-}
-
-console.log(
-   mostWordsFound([
-      'alice and bob love leetcode hey there',
-      'i think so too',
-      'this is great thanks very much',
-   ])
-)
