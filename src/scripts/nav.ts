89bb5e948a463c107c140b9f0ac139ab4b17f3ee
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 14:42:27 +0000

    leetcode shuffle array setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,3 +15,8 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
+
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7]
