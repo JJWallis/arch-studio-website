17517babfa4a4fb956e88f168f8d871d8743a71a
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 15:16:24 +0000

    leetcode shuffle array remove

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,23 +15,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
-
-// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
-// Input: nums = [2,5,1,3,4,7], n = 3
-// Output: [2,3,5,4,1,7]
-
-function shuffle(nums: number[], n: number) {
-   const results = []
-
-   let i = n
-   for (let j = 0; j < n; j++) {
-      results.push(nums[j])
-      results.push(nums[i])
-      i++
-   }
-
-   return results
-}
-
-console.log(shuffle([2, 5, 1, 3, 4, 7], 3))
