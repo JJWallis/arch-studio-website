179f9c9f48052e333a1da040437392dcf2d3594f
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-18 16:08:46 +0000

    leetcode smallerNumsThanCurr removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,14 +15,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
-
-// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it
-// That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i]
-// Return the answer in an array
-// In order to improve the time complexity, we can sort the array and get the answer for each array element
-
-function smallerNumbersThanCurrent(nums: number[]): number[] {
-   return nums.sort()
-}
-
-console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]))
