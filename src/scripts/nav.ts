6c313fc6aee46192d987f04021f993854a772441
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-19 14:00:01 +0000

    leetcode - encode removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,3 +15,13 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
+
+// You are given a string s and an integer array indices of the same length
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string
+// Return the shuffled string
+
+function restoreString(s: string, indices: number[]): string {
+   return indices.map((num) => s[num]).join('')
+}
+
+console.log(restoreString('codeleet', [4, 5, 6, 7, 0, 2, 1, 3])) // 'leetcode'
