c6c3899380cb83acd1563cb9997625ab21165a00
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-21 10:41:02 +0000

    leetcode countMatches setuo

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,3 +15,19 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
+
+// You are given an array items, where each items[i] = [typei, colori, namei]
+// You are also given a rule represented by two strings, ruleKey and ruleValue
+// The ith item is said to match the rule if one of the following is true:
+//  ruleKey == "type" and ruleValue == typei
+//  ruleKey == "color" and ruleValue == colori
+//  ruleKey == "name" and ruleValue == namei
+// Return the number of items that match the given rule
+
+function countMatches(
+   items: string[][],
+   ruleKey: string,
+   ruleValue: string
+): number {
+   return 0
+}
