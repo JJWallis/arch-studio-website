44db4aec5ea7de4f85d660fcb22517579b08ac85
JJWallis <josh.jamesonwallis@gmail.com>
2022-02-21 10:14:29 +0000

    target arr - solution ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -17,12 +17,12 @@ hamburger.addEventListener('click', () => {
 // review solution - better approach?, can go back if run into issue
 
 // Given two arrays of integers nums and index. Your task is to create target array under the following rules:
-// Initially target array is empty.
-// From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
-// Repeat the previous step until there are no elements to read in nums and index.
-// Return the target array.
+// Initially target array is empty
+// From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array
+// Repeat the previous step until there are no elements to read in nums and index
+// Return the target array
 
-function createTargetArray(nums: number[], index: number[]): number[] {
+function createTargetArray(nums: number[], index: number[]) {
    const target = []
    return target
 }
