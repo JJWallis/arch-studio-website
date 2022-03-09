a260709d7d37d1b3c13f1c4416df66158d324210
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-09 16:51:02 +0000

    maxDepth leetcode removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -15,26 +15,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach?, can go back if run into issue
-
-function maxDepth(s: string): number {
-   const strArr = s.split('')
-   const results = strArr
-      .map((_, idxOuter) => {
-         let leftTotal = 0
-         let rightTotal = 0
-         return strArr
-            .map((valInner, idxInner) => {
-               valInner === '('
-                  ? leftTotal++
-                  : valInner === ')'
-                  ? rightTotal++
-                  : null
-               if (idxOuter === idxInner) return leftTotal - rightTotal
-            })
-            .filter((val) => val !== undefined)
-      })
-      .flat(1)
-   return Math.max(...results)
-}
-
-console.log(maxDepth('(1+(2*3)+((8)/4))+1')) // 3
