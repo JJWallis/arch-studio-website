21b43de3b38728106279bb382fb2612d748b339a
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-14 15:17:16 +0000

    leetcode repeatedNTimes setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -17,16 +17,11 @@ hamburger.addEventListener('click', () => {
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
 
-function cellsInRange(s: string): string[] {
-   const [start, end] = s.split(':')
-   // declare alphabet str/arr
-   // letter range = find() starting letter + retrieve index | find() index of ending letter too
-   // number range = + Math.min of starting number + Math.max of ending number
-   // this will determine how many times we iterate over each letter + push(letterWithCurrNum) to resulting arr - including starting/ending num
-   return []
+function repeatedNTimes(nums: number[]): number {
+   return 0
 }
 
-console.log(cellsInRange('K1:L2')) // ['K1', 'K2', 'L1', 'L2']
+console.log(repeatedNTimes([1, 2, 3, 3])) // 3
 
 // const initialState = [
 //    { name: 'name', value: '' },
