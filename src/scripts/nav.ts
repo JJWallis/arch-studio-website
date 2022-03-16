01b67324d0a9caea769cb4b6c5887f317f917b27
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-16 14:07:32 +0000

    leetcode minTimeToVisist setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -16,3 +16,17 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
+
+//
+
+function minTimeToVisitAllPoints(points: number[][]): number {
+   return 0
+}
+
+console.log(
+   minTimeToVisitAllPoints([
+      [1, 1],
+      [3, 4],
+      [-1, 0],
+   ])
+)
