9a22ec91e081bbca52c43239ef0de222ba15ebd3
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-17 13:24:50 +0000

    leetcode - xor setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -17,33 +17,8 @@ hamburger.addEventListener('click', () => {
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
 
-// On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi].
-// Return the minimum time in seconds to visit all the points in the order given by points.
-// You can move according to these rules:
-// In 1 second, you can either:
-//  move vertically by one unit,
-//  move horizontally by one unit, or
-//  move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
-// You have to visit the points in the same order as they appear in the array.
-// You are allowed to pass through points that appear later in the order, but these do not count as visits.
+function xorOperation(n: number, start: number): number {
+   return 0
+}
 
-const minTimeToVisitAllPoints = (points) =>
-   points
-      .slice(0, points.length - 1)
-      .reduce(
-         (acc, [x, y], idx) =>
-            acc +
-            Math.max(
-               Math.abs(x - points[idx + 1][0]),
-               Math.abs(y - points[idx + 1][1])
-            ),
-         0
-      )
-
-console.log(
-   minTimeToVisitAllPoints([
-      [1, 1],
-      [3, 4],
-      [-1, 0],
-   ])
-)
+console.log(xorOperation(5, 0))
