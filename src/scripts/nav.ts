233ada45e748fe115521f23b34073ce8cc449505
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-17 13:40:01 +0000

    leetcode - xor comments setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -17,14 +17,13 @@ hamburger.addEventListener('click', () => {
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
 
-// You are given an integer n and an integer start.
-// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
-// Return the bitwise XOR of all elements of nums.You are given an integer n and an integer start.
 // Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
 // Return the bitwise XOR of all elements of nums.
 
 function xorOperation(n: number, start: number): number {
-   return 0
+   const results: number[] = []
+   for (let i = 0; i < n; i++) results.push(start + 2 * i)
+   return results.reduce((acc, curr, idx) => (acc = curr ^ results[idx + 1]), 0)
 }
 
-console.log(xorOperation(5, 0))
+console.log(xorOperation(4, 3)) // 8
