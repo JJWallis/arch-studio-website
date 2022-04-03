fd80c56f33f8bf2e1895580d274b6ec0497c0c1c
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-03 13:10:47 +0100

    codewars - alphabetPosition setup

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -17,18 +17,14 @@ hamburger.addEventListener('click', () => {
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
 
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23
-// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in
-// Additionally, if the number is negative, return 0 (for languages that do have them)
-// Note: If the number is a multiple of both 3 and 5, only count it once
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc. = 26 (not zero indexed)
 
-function solution(num: number) {
-   if (num < 0) return 0
-   let total = 0
-   for (let i = 1; i < num; i++) {
-      if (i % 3 === 0 || i % 5 === 0) total += i
-   }
-   return total
+function alphabetPosition(text: string) {
+   const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+   const textSplit = text.toLowerCase().split(' ')
+   return text
 }
 
-console.log(solution(10)) // 23
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
