5a1931d0a5a179a0932d9663d57c797e9f7c8ae0
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-03 13:15:32 +0100

    codewars - alphabetPosition solution pre filtering empty spaces

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -18,13 +18,16 @@ hamburger.addEventListener('click', () => {
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
 
 // In this kata you are required to, given a string, replace every letter with its position in the alphabet.
-// If anything in the text isn't a letter, ignore it and don't return it.
+// If anything in the text isn't a letter, ignore it and don't return it. continue statement
 // "a" = 1, "b" = 2, etc. = 26 (not zero indexed)
 
 function alphabetPosition(text: string) {
-   const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-   const textSplit = text.toLowerCase().split(' ')
-   return text
+   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+   const textSplit = text.toLowerCase().split('')
+   const results = textSplit.map(
+      (letter) => alphabet.findIndex((l) => l === letter) + 1
+   )
+   return results
 }
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
