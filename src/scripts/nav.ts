60d91ece26cab62cbde0aa494fa7b89e3ba649e3
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-21 08:33:51 +0100

    slice bug fixed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -19,9 +19,13 @@ function findMiddleIndex(nums: number[]) {
    for (let i = 0; i < nums.length; i++) {
       const curr = nums[i]
       const leftArr = nums.slice(0, i)
-      const rightArr = nums.slice(i, nums.length - 1)
+      const rightArr = nums.slice(i + 1, nums.length)
 
-      if (!leftArr.length || !rightArr.length) continue
+      console.log(leftArr, rightArr)
+
+      if (i === 0) return
+
+      // if (!leftArr.length || !rightArr.length) continue
    }
 }
 
