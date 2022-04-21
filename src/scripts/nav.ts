af6bd03a0bd27c93478fcd7580d2e7326b5b7275
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-21 08:07:10 +0100

    prev chal removed

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -14,26 +14,3 @@ hamburger.addEventListener('click', () => {
 // execute plan - from outline of plan via comments
 // what do I currently know
 // review solution - better approach? + `refactor` for readability, extensibility, re-usability, and performance
-
-function countWords(words1: string[], words2: string[]): number {
-   const words1Str = words1.join(' ')
-   const words2Str = words2.join(' ')
-   let total = 0
-
-   words1.forEach((word) => {
-      const regex = new RegExp(word, 'gi')
-      const word1Match = words1Str.match(regex)
-      const word2Match = words2Str.match(regex)
-      const totalLength = word1Match?.length + word2Match?.length
-      if (totalLength === 2) total += 1
-   })
-
-   return total
-}
-
-console.log(
-   countWords(
-      ['leetcode', 'is', 'amazing', 'as', 'is'],
-      ['amazing', 'leetcode', 'is']
-   )
-)
